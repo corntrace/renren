3c2bb0337f5a5530abeac7a6a3b6480fc3c3efd5
Kevin Fu <corntrace@gmail.com>
2010-05-12 23:30:42 +0800

    According to the Renren signature rule(http://is.gd/c5WJN), the method posting params to renren restapi server has been changed. Now the params include the :sig key and value.

--- lib/renren/service.rb
+++ lib/renren/service.rb
@@ -1,12 +1,18 @@
 require 'net/http'
+require 'digest/md5'
 require 'pp'
+
 module Renren
   class Service
     DEBUG = false
 
     def post(params)
       pp "### Posting Params: #{params.inspect}" if DEBUG
-      Net::HTTP.post_form(url, params)
+	  sorted_params_str_with_secret_key = params.sort{|a,b| a.to_s <=> b.to_s}.map do |pair|
+        "#{pair.first}=#{pair.last}"
+      end.join('') << ::Renren.secret_key
+      sig = Digest::MD5.hexdigest sorted_params_str_with_secret_key
+      Net::HTTP.post_form(url, params.merge(:sig => sig))
     end
 
     private
@@ -16,3 +22,4 @@ module Renren
     end
   end
 end
+
